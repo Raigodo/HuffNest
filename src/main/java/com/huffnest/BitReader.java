582f56ac0fc1feb2f7d01f4d310aa6a1fe071bbb
Valters <103326282+Raigodo@users.noreply.github.com>
2026-02-20 03:47:54 +0200

    bit reader

---
+++ src/main/java/com/huffnest/BitReader.java
@@ -0,0 +1,95 @@
+package com.huffnest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class BitReader {
+
+  public BitReader(String path) throws IOException {
+    stream = Files.newInputStream(Paths.get(path));
+    fillBuffer();
+    currentByte = queue.next();
+    isCurrentByteActive = true;
+  }
+
+  private InputStream stream;
+  private boolean closed = false;
+
+  private static final int BUFFER_SIZE = 2;
+  private static final int BUFFER_QUEUE_SIZE = BUFFER_SIZE + 1;
+  private byte[] _buffer = new byte[BUFFER_SIZE];
+  private byte[] _bufferTemp = new byte[1];
+  private ByteRingBuffer queue = new ByteRingBuffer(BUFFER_QUEUE_SIZE);
+  private byte currentByte = 0;
+  private byte bitIndex = 0;
+  private boolean isCurrentByteActive = false;
+
+  public boolean isClosed() {
+    return closed && queue.size() == 0 && !isCurrentByteActive;
+  }
+
+  public byte nextBit() throws IOException {
+    if (isClosed()) throw new IOException("Stream is closed");
+    byte bit = (byte) ((currentByte >> (7 - bitIndex)) & 1);
+    bitIndex = (byte) ((bitIndex + 1) % 8);
+    if (bitIndex == 0) {
+      currentByte = queue.next();
+      isCurrentByteActive = true;
+    }
+
+    return bit;
+  }
+
+  public byte nextByte() throws IOException {
+    if (isClosed()) throw new IOException("Stream is closed");
+    if (bitIndex > 0 && queue.size() <= 0) {
+      throw new IOException("Stream is closed");
+    }
+
+    if (queue.size() <= 0 && !closed) {
+      fillBuffer();
+    }
+
+    if (isCurrentByteActive && queue.size() == 0) {
+      isCurrentByteActive = false;
+      return currentByte;
+    }
+
+    byte nextByte = queue.next();
+
+    if (bitIndex == 0) {
+      byte toReturn = currentByte;
+      currentByte = nextByte;
+      return toReturn;
+    }
+
+    byte forgedByte = (byte) (currentByte << bitIndex);
+    byte nextByteShifted = (byte) (nextByte >> (8 - bitIndex));
+    forgedByte |= nextByteShifted;
+
+    return forgedByte;
+  }
+
+  private void fillBuffer() throws IOException {
+    if (isClosed()) throw new IOException("Stream is closed");
+    int read = stream.read(_buffer, 0, BUFFER_SIZE - 1);
+    for (int i = 0; i < read; i++) queue.add(_buffer[i]);
+    if (read < BUFFER_SIZE - 1) {
+      close();
+      return;
+    }
+    read = stream.read(_bufferTemp, 0, 1);
+    if (read <= 0) {
+      close();
+      return;
+    }
+    queue.add(_bufferTemp[0]);
+  }
+
+  public void close() throws IOException {
+    closed = true;
+    stream.close();
+  }
+}
--- src/main/java/com/huffnest/Main.java
+++ src/main/java/com/huffnest/Main.java
@@ -4,5 +4,15 @@ import java.io.IOException;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {}
+  public static void main(String[] args) throws IOException {
+    BitReader br = new BitReader(
+      "E:\\Projects\\huffnest\\src\\main\\resources\\test.txt"
+    );
+    for (int i = 0; i < 8; i++) {
+      System.out.println("!: " + br.nextBit());
+    }
+    while (!br.isClosed()) {
+      System.out.println("!: " + (char) br.nextByte());
+    }
+  }
 }
