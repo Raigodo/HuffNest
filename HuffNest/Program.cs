a38146355150c6cf15b0e21bc8931edcd11bbc67
Valters <103326282+Raigodo@users.noreply.github.com>
2026-02-22 17:54:42 +0200

    initial c# project

---
+++ HuffNest/Program.cs
@@ -0,0 +1,7 @@
+﻿using (FileStream fs = new FileStream("E:\\Projects\\huffnest\\test.txt", FileMode.Open))
+{
+    byte[] buffer = new byte[10];
+    int bytesRead = fs.Read(buffer, 0, 10);
+    for (int i = 0; i < bytesRead; i++)
+        Console.WriteLine((char)buffer[i]);
+}
